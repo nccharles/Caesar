d81adf0e061f95e87db3813465eba0e821d9e046
Charles NDAYISABA <nccharles1@gmail.com>
2020-11-15 18:44:20 +0200

    jenkins tes

--- src/test/java/models/EncodeTest.java
+++ src/test/java/models/EncodeTest.java
@@ -12,17 +12,17 @@ public class EncodeTest {
     }
 
     @Test
-    public void newEncode_getMessage() {
+    public void newEncode_getMessage() throws Exception {
         Encode testEncode = new Encode("abc", 1);
         assertEquals("abc", testEncode.getMsg());
     }
     @Test
-    public void newEncode_getKey() {
+    public void newEncode_getKey() throws Exception {
         Encode testEncode = new Encode("abc", 1);
         assertEquals(1, testEncode.getKey());
     }
     @Test
-    public void newEncode_Encrypt_Message() {
+    public void newEncode_Encrypt_Message()throws Exception  {
         Encode testEncode = new Encode("abc", 1);
         assertEquals("bcd", testEncode.strEncrypt());
     }
